dd33b9a05e0eec25874072007b2fb0d4034902de
ZUIREZ <rashedxshawon@gmail.com>
2024-05-21 22:28:21 +0600

    Uploaded

---
+++ CodeHelp/Patterns/pattern21.cpp
@@ -0,0 +1,27 @@
+/*
+
+*****
+****
+***
+**
+*
+
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+
+    for(int i=1; i<=n; i++)
+    {
+        for(int j=n-i+1; j>0; j--)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
