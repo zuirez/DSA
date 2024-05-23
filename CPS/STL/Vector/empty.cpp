37d313a10f7652879cd54cb25c7676da69d7a517
ZUIREZ <rashedxshawon@gmail.com>
2024-05-23 11:57:35 +0600

    Upload

---
+++ CPS/STL/Vector/empty.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    vector<int> v={1,2,3,4,5};
+    vector<int> v2;
+    
+    cout<<"Is vector 1 empty : "<<v.empty()<<endl;
+    cout<<"Is vector 2 empty : "<<v2.empty();
+}
