4d6ae652bd7ddfc4120a5550512b24bf3d9e842f
ZUIREZ <rashedxshawon@gmail.com>
2024-05-23 21:46:06 +0600

    modified

--- CPS/STL/Vector/forEachIterator.cpp
+++ CPS/STL/Vector/forEachIterator.cpp
@@ -6,5 +6,16 @@ int main()
     vector<int> v={2,4,6,8};
 
     cout<<"Vector : ";
-    for (int u : v) cout<<u<<" ";
+    for (int u : v) 
+    {
+        cout<<u<<" ";
+    }
+    cout<<endl;
+    
+    cout<<"Changed : ";
+    for (int u : v) 
+    {
+        u+=2;
+        cout<<u<<" ";
+    }
 }
