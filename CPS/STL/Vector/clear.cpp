7c22799ee6f364c00c117306b2adf71cd9f032a1
ZUIREZ <rashedxshawon@gmail.com>
2024-05-23 11:54:30 +0600

    Upload

---
+++ CPS/STL/Vector/clear.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    vector<int> v={1,2,3,4,5};
+    cout<<"Size before clear : "<<v.size()<<endl;
+
+    v.clear();
+    cout<<"Size after clear : "<<v.size()<<endl;
+}
