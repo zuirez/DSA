e3f8a8126ef119d88448d7793d090badfe8d687a
ZUIREZ <rashedxshawon@gmail.com>
2024-05-23 12:09:21 +0600

    modified

--- CPS/STL/Vector/vector.cpp
+++ CPS/STL/Vector/vector.cpp
@@ -3,8 +3,10 @@ using namespace std;
 
 int main()
 {
+    //Creating vectors
     vector<int> v;
     vector<int> v2={3,5,7,9};
+    vector<int> v3(10);
 
     //Adding 5,10,15,20 in vectors index
     v.push_back(5);
@@ -13,20 +15,32 @@ int main()
     v.push_back(20);
 
     //The size of a vector
-    cout<<"Size of vector : "<<v.size()<<endl;
-    cout<<v[0]<<endl;
-    cout<<v[1]<<endl;
-    cout<<v[2]<<endl;
+    cout<<"Size of vector 1 : "<<v.size()<<endl;
+    cout<<"Vector 1 : ";
+    cout<<v[0]<<" ";
+    cout<<v[1]<<" ";
+    cout<<v[2]<<" ";
     cout<<v[3]<<endl;    
 
     //Changing an existing index of vector v
     v[1]=100;
     cout<<"Changing index 1 : ";
-    cout<<v[1];
+    cout<<v[1]<<endl;
 
     //Printing vector v2
+    cout<<"Size of vector 2 : "<<v2.size()<<endl;
+    cout<<"Vector 2 : ";
     for(int i=0; i<v2.size(); i++)
     {
-        cout<<v2[i]<<endl;
+        cout<<v2[i]<<" ";
+    }
+    cout<<endl;
+
+    //Printing vector v3
+    cout<<"Size of vector 3 : "<<v3.size()<<endl;
+    cout<<"Vector 3 : ";
+    for(int i=0; i<v3.size(); i++)
+    {
+        cout<<v3[i]<<" ";
     }
 }
