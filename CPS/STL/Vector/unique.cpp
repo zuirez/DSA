950e5c84eca63cf0469ecf41cad1ee8371767e04
ZUIREZ <rashedxshawon@gmail.com>
2024-05-24 11:07:41 +0600

    New file

---
+++ CPS/STL/Vector/unique.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    vector<int> v={3,5,1,7,1,6,4,4,2,9,3,3};
+
+    //sorting
+    sort(v.begin(), v.end());
+
+    //function to find unique (distinct) elements
+    unique(v.begin(), v.end());
+
+    //printing the vector
+    for(auto u:v) cout<<u<<" ";
+    cout<<endl;
+}
