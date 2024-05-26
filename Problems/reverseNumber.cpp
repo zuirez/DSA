f5f3122e0f15f9b1e3823ae8f9bfbe7dfa72929a
ZUIREZ <rashedxshawon@gmail.com>
2024-05-26 14:50:17 +0600

    New file

---
+++ Problems/reverseNumber.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n,reverse=0;
+    cin>>n;
+
+    while (n!=0)
+    {
+        int last = n%10;
+        n=n/10;
+
+        reverse=reverse*10;
+        reverse=reverse+last;
+    }
+    cout<<reverse;
+}
