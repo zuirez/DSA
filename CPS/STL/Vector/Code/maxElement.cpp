7e1b66492f232c34d8118ba2d8fd1adbb8d89294
ZUIREZ <rashedxshawon@gmail.com>
2024-05-28 22:54:43 +0600

    modified

--- CPS/STL/Vector/Code/maxElement.cpp
+++ CPS/STL/Vector/Code/maxElement.cpp
@@ -5,9 +5,9 @@ int main()
 {
     vector<int> v={3,5,1,7,1,6,4,4,2,9,3,3};
 
-    //max_element function returns an iterator.
+    //Max_element function returns an iterator.
     vector<int>:: iterator i= max_element(v.begin(), v.end());
 
-    //to print the element of the address the max_elements return we have to use a *
+    //To print the element of the address the max_elements return we have to use a *
     cout<<*i;
 }
