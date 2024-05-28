3afcbdb6469ce4fe127c376e76b40257b907db20
ZUIREZ <rashedxshawon@gmail.com>
2024-05-28 22:53:11 +0600

    modified

--- CPS/STL/Vector/Code/backAndPopBack.cpp
+++ CPS/STL/Vector/Code/backAndPopBack.cpp
@@ -5,15 +5,15 @@ int main()
 {
     vector<int> v={5,7,3,4,6};
 
-    cout<<v.back()<<endl; //printing the last element of the vector
+    cout<<v.back()<<endl; //printing the last element of the vector.
 
-    v.pop_back(); //removing the last element of the vector
+    v.pop_back(); //removing the last element of the vector.
 
-    cout<<v.back()<<endl; //printing the last element of the vector
+    cout<<v.back()<<endl; //printing the last element of the vector.
 
-    v.pop_back(); //removing the last element of the vector
+    v.pop_back(); //removing the last element of the vector.
     
-    cout<<v.back()<<endl; //printing the last element of the vector
+    cout<<v.back()<<endl; //printing the last element of the vector.
     
-    v.pop_back(); //removing the last element of the vector
+    v.pop_back(); //removing the last element of the vector.
 }
