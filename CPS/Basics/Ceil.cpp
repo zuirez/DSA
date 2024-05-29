62e114658943bb5ecc7e420a4b3c63c87b152d82
ZUIREZ <rashedxshawon@gmail.com>
2024-05-29 20:01:17 +0600

    new file:   CPS/Basics/Ceil.cpp

---
+++ CPS/Basics/Ceil.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    float a=3.00;
+    float b=3.10;
+    float c=3.49;
+    double d=3.50;
+    double e=3.99;
+    double f=4.0;
+
+    cout<<"a : "<<ceil(a)<<endl;
+    cout<<"b : "<<ceil(b)<<endl;
+    cout<<"c : "<<ceil(c)<<endl;
+    cout<<"d : "<<ceil(d)<<endl;
+    cout<<"e : "<<ceil(e)<<endl;
+    cout<<"f : "<<ceil(f)<<endl;
+}
