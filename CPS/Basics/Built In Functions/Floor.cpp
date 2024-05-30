bcc752dfcb5c0ac139a8d2682c274e20b2dc4b20
ZUIREZ <rashedxshawon@gmail.com>
2024-05-30 09:38:28 +0600

    new file:   CPS/Basics/Built In Functions/Floor.cpp

---
+++ CPS/Basics/Built In Functions/Floor.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    float a=3.00;
+    float b=3.10;
+    float c=3.49;
+    double d=3.50;
+    double e=3.99;
+    double f=4.0;
+
+    cout<<"a : "<<floor(a)<<endl;
+    cout<<"b : "<<floor(b)<<endl;
+    cout<<"c : "<<floor(c)<<endl;
+    cout<<"d : "<<floor(d)<<endl;
+    cout<<"e : "<<floor(e)<<endl;
+    cout<<"f : "<<floor(f)<<endl;
+}
