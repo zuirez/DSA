f70403aa18540265db6569e5638f44c302dc5920
ZUIREZ <rashedxshawon@gmail.com>
2024-05-30 09:37:12 +0600

    modified:   CPS/STL/Vector/Code/maxElement.cpp

--- CPS/STL/Vector/Code/maxElement.cpp
+++ CPS/STL/Vector/Code/maxElement.cpp
@@ -9,5 +9,17 @@ int main()
     vector<int>:: iterator i= max_element(v.begin(), v.end());
 
     //To print the element of the address the max_elements return we have to use a *
-    cout<<*i;
+    cout<<*i<<endl;
+
+
+    //Another way to find max
+    vector<int> v2={3,5,1,7,1,6,4,4,2,9,3,3};
+
+    auto max = max_element(v2.begin(),v2.end());
+    cout<<*max<<endl;
+
+
+    //BEST WAY TO FIND MAX
+    vector<int> v3 = {88,57,1,5,99,48,4,18};
+    cout<<*max_element(v3.begin(),v3.end());
 }
