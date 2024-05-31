2a6824a0c7b2a17858520730bba4b7be056ed883
ZUIREZ <rashedxshawon@gmail.com>
2024-05-31 10:07:08 +0600

    new file:   CPS/Basics/Built In Functions/Swap.cpp

---
+++ CPS/Basics/Built In Functions/Swap.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a=5, b=2;
+
+    //swaping 2 numbers
+    swap(a,b);
+
+    //printing
+    cout<<"a : "<<a<<endl<<"b : "<<b; 
+}
