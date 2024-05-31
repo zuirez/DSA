e228ae0f2b98a3df630ed5213efca9ce06d62cff
ZUIREZ <rashedxshawon@gmail.com>
2024-05-31 10:07:28 +0600

    new file:   CPS/STL/String/concatenation.cpp

---
+++ CPS/STL/String/concatenation.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string s1 = "Rashedul";
+    string s2 = "Islam";
+    string s3 = "Shawon";
+    string s = " ";
+
+    string s4 = s1+s+s2+s+s3;
+
+    cout<<s4;
+}   
