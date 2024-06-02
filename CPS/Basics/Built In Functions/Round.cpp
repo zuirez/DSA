e99e4d2e8d294fcf72364f0a6e4d90ddcd2a01bf
ZUIREZ <rashedxshawon@gmail.com>
2024-06-02 09:10:44 +0600

    new file:   CPS/Basics/Built In Functions/Round.cpp

---
+++ CPS/Basics/Built In Functions/Round.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    float a=3.00;
+    float b=3.10;
+    float c=3.49;
+    double d=3.50;
+    double e=3.99;
+    double f=4.0;
+
+    cout<<"a : "<<round(a)<<endl;
+    cout<<"b : "<<round(b)<<endl;
+    cout<<"c : "<<round(c)<<endl;
+    cout<<"d : "<<round(d)<<endl;
+    cout<<"e : "<<round(e)<<endl;
+    cout<<"f : "<<round(f)<<endl;
+}
