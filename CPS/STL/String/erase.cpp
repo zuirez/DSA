5b299c25bbba16989f743f217708c0ccef2417ff
ZUIREZ <rashedxshawon@gmail.com>
2024-06-02 09:12:33 +0600

    new file:   CPS/STL/String/erase.cpp

---
+++ CPS/STL/String/erase.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define optimize() ios_base::sync_with_stdio(0);//cin.tie(0);cout.tie(0);
+#define endl '\n'
+
+int main()
+{
+    optimize();
+
+    string s= "BdaeCcbA";
+    
+    //erasing first char
+    s.erase(s.begin());
+    cout<<s<<endl;
+
+    //erasing last char
+    s.erase(s.end()-1);
+    cout<<s<<endl;
+}
