d85e8688125a5630b7c783481f1f48900f27f343
ZUIREZ <rashedxshawon@gmail.com>
2024-06-02 09:11:48 +0600

    new file:   CPS/STL/String/empty.cpp

---
+++ CPS/STL/String/empty.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string s = "Rashed";
+
+    cout<<s.empty()<<endl; // not empty, returns 0
+
+    string s1;
+
+    cout<<s1.empty()<<endl; // empty, returns 1
+}
