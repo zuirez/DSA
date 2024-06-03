3f0aa5bcc924c764059aa909f5806341ad5a89a6
ZUIREZ <rashedxshawon@gmail.com>
2024-06-03 09:15:32 +0600

    new file:   CPS/STL/Vector/Code/IndexOfMin.cpp

---
+++ CPS/STL/Vector/Code/IndexOfMin.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    vector<int> v={3,5,1,6,4,4,2,9,3,3};
+
+    //index of the maximum number
+    int i= min_element(v.begin(), v.end()) - v.begin();
+
+    cout<<i;
+}
