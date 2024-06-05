ea30ab993bbfb218a892cc2a47665333c0f185c9
ZUIREZ <rashedxshawon@gmail.com>
2024-06-05 10:13:14 +0600

    new file:   CPS/STL/String/reverse.cpp

---
+++ CPS/STL/String/reverse.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string s = "Shawon";
+
+    reverse(s.begin(), s.end());
+
+    cout<<s<<endl;
+
+    string s1;
+    cin>>s1;
+    reverse(s1.begin(),s1.end());
+    cout<<s1;
+}
